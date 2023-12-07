87cb800d476b68c0c3104132784d3fe113eaa1d0
yujuyamelong <kikiblee@naver.com>
2023-12-07 12:43:39 +0900

    Member 엔티티 테이블(name)추가 및 MemberDTO 생성

--- src/main/java/com/ditto/entity/Member.java
+++ src/main/java/com/ditto/entity/Member.java
@@ -1,14 +1,17 @@
 package com.ditto.entity;
 
 import com.ditto.constant.Role;
+import com.ditto.dto.MemberFormDTO;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
 @Entity
+@Table(name="member")
 @Getter @Setter @ToString
 public class Member extends BaseEntity{
 
@@ -22,7 +25,9 @@ public class Member extends BaseEntity{
     @Column(nullable = false)
     private String name;
 
-    private int phoneNum;
+    private int phoneNum1;
+
+    private int phoneNum2;
 
     private String address;
 
@@ -32,4 +37,23 @@ public class Member extends BaseEntity{
     private Role role;
     
     //엔티티 생성 메서드 추가할 것
+    public static Member createMember(MemberFormDTO memberFormDTO,
+                                      PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+        member.setId(memberFormDTO.getId());
+        member.setName(memberFormDTO.getName());
+        member.setPhoneNum1(memberFormDTO.getPhoneNum1());
+        member.setPhoneNum2(memberFormDTO.getPhoneNum2());
+        member.setAddress(memberFormDTO.getAddress());
+        member.setEmail(memberFormDTO.getEmail());
+        member.setRole(Role.USER);
+
+        //패스워드에 관한 인코더
+        String password = passwordEncoder.encode(memberFormDTO.getPassword());
+        member.setPassword(password);
+
+        return member;
+
+
+    }
 }
