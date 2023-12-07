6a12a3578a6925cfa8a336e8f52b3052094ee897
eogml0722 <eogml0722@naver.com>
2023-12-07 17:17:08 +0900

    /board/** 인증 추가 (임시)

--- src/main/java/com/ditto/config/SecurityConfig.java
+++ src/main/java/com/ditto/config/SecurityConfig.java
@@ -2,14 +2,9 @@ package com.ditto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,39 +12,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
-        http.formLogin()
-                .loginPage("/members/login") // 로그인 페이지로 설정
-                .defaultSuccessUrl("/") // 성공시 보여줄 url
-                .usernameParameter("email") // 로그인시 사용할 파라미터 이름
-                .failureUrl("/members/login/error") // 로그인 실패시 보여줄url
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/"); // 로그아웃 성공시 보여줄 url
-
         http.authorizeRequests()
                 .mvcMatchers("/").permitAll()
                 .mvcMatchers("/css/**", "/js/**", "/img/**", "/extras/**").permitAll()
-                .mvcMatchers("/members/**").permitAll()
                 .anyRequest().authenticated();
-
-        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
-
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration
-    ) throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
 
 }
