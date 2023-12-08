8c633820cf3e17a0d0237a775f112d1c48d12513
yujuyamelong <kikiblee@naver.com>
2023-12-08 15:19:28 +0900

    회원가입 css 수정, js 적용

--- src/main/java/com/ditto/entity/Board.java
+++ src/main/java/com/ditto/entity/Board.java
@@ -1,65 +1,67 @@
-package com.ditto.entity;
-
-
-import com.ditto.constant.BoardCategory;
-import com.ditto.dto.BoardDTO;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import org.modelmapper.ModelMapper;
-import org.springframework.web.accept.MappingMediaTypeFileExtensionResolver;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Getter @Setter @ToString
-public class Board extends BaseEntity {
-    @Id
-    @Column(name = "board_id")
-    @GeneratedValue
-    private Long id;
-
-    //멤버는 BaseEntity로 대체
-
-    @Column(nullable = false)
-    private String title;
-
-    private String content;
-
-    //조회수
-    @Column(nullable = false)
-    private int viewCount;
-
-    //게시판 분류
-    private BoardCategory boardCategory;
-
-    //사용자 정보
-    private Member member;
-    
-    
-    @OneToMany(mappedBy = "board")
-    //이미지 업로드 시 사용
-    private List<Img> imgList = new ArrayList<>();
-
-
-
-
-    //static 아니면 생성자로 엔티티인스턴스를 생성해서 메서드를 써야한다.
-
-
-    public static Board createBoard(Long id, String title, String content,
-                 BoardCategory boardCategory, Member member, List<Img> imgList) {
-        Board board = new Board();
-
-        board.setId(id);
-        board.setTitle(title);
-        board.setContent(content);
-        board.setBoardCategory(boardCategory);
-        board.setMember(member);
-        board.setImgList(imgList);
-
-        return board;
-    }
-}
+//package com.ditto.entity;
+//
+//
+//import com.ditto.constant.BoardCategory;
+//import com.ditto.dto.BoardDTO;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.web.accept.MappingMediaTypeFileExtensionResolver;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Getter @Setter @ToString
+//public class Board extends BaseEntity {
+//    @Id
+//    @Column(name = "board_id")
+//    @GeneratedValue
+//    private Long id;
+//
+//    //멤버는 BaseEntity로 대체
+//
+//    @Column(nullable = false)
+//    private String title;
+//
+//    private String content;
+//
+//    //조회수
+//    @Column(nullable = false)
+//    private int viewCount;
+//
+//    //게시판 분류
+//    private BoardCategory boardCategory;
+//
+//    //사용자 정보
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="member_id")
+//    private Member member;
+//
+//
+//    @OneToMany(mappedBy = "board")
+//    //이미지 업로드 시 사용
+//    private List<Img> imgList = new ArrayList<>();
+//
+//
+//
+//
+//    //static 아니면 생성자로 엔티티인스턴스를 생성해서 메서드를 써야한다.
+//
+//
+//    public static Board createBoard(Long id, String title, String content,
+//                 BoardCategory boardCategory, Member member, List<Img> imgList) {
+//        Board board = new Board();
+//
+//        board.setId(id);
+//        board.setTitle(title);
+//        board.setContent(content);
+//        board.setBoardCategory(boardCategory);
+//        board.setMember(member);
+//        board.setImgList(imgList);
+//
+//        return board;
+//    }
+//}
--- src/main/java/com/ditto/service/BoardService.java
+++ src/main/java/com/ditto/service/BoardService.java
@@ -1,21 +1,21 @@
-package com.ditto.service;
-
-
-import com.ditto.entity.Board;
-import com.ditto.repository.BoardRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class BoardService {
-
-    private final BoardRepository boardRepository;
-
-    public void insertBoard(Board board){
-        boardRepository.save(board);
-    }
-
-}
+//package com.ditto.service;
+//
+//
+//import com.ditto.entity.Board;
+//import com.ditto.repository.BoardRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//public class BoardService {
+//
+//    private final BoardRepository boardRepository;
+//
+//    public void insertBoard(Board board){
+//        boardRepository.save(board);
+//    }
+//
+//}
