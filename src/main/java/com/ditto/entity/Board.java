5aba282f5e02400af20fe191838da5612e2f4a3a
eogml0722 <eogml0722@naver.com>
2023-12-08 17:09:33 +0900

    @Table 추가

--- src/main/java/com/ditto/entity/Board.java
+++ src/main/java/com/ditto/entity/Board.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "board")
 @Getter @Setter @ToString
 public class Board extends BaseEntity {
     @Id
@@ -29,38 +30,39 @@ public class Board extends BaseEntity {
     private String content;
 
     //조회수
-    @Column(nullable = false)
     private int viewCount;
 
     //게시판 분류
     private BoardCategory boardCategory;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     //사용자 정보
     private Member member;
     
-    
-    @OneToMany(mappedBy = "board")
+    /*
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY)
     //이미지 업로드 시 사용
     private List<Img> imgList = new ArrayList<>();
-
+    */
 
 
 
     //static 아니면 생성자로 엔티티인스턴스를 생성해서 메서드를 써야한다.
 
 
-    public static Board createBoard(Long id, String title, String content,
-                 BoardCategory boardCategory, Member member, List<Img> imgList) {
-        Board board = new Board();
-
-        board.setId(id);
-        board.setTitle(title);
-        board.setContent(content);
-        board.setBoardCategory(boardCategory);
+    public static Board createBoard(BoardDTO boardDTO, Member member) {
+//        Board board = new Board();
+//
+//        board.setId(boardDTO.getId());
+//        board.setTitle(boardDTO.getTitle());
+//        board.setContent(board.getContent());
+//        board.setBoardCategory(boardDTO.getBoardCategory());
+        //멤버와 보드 연결
+        //modelMapper 사용
+        Board board = boardDTO.createBoard();
         board.setMember(member);
-        board.setImgList(imgList);
 
         return board;
     }
