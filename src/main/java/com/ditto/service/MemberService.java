ce5b7c637e7e4418ddb50d178f8fa0d92b27a406
yujuyamelong <kikiblee@naver.com>
2023-12-08 14:53:07 +0900

    회원가입 css 수정, js 적용

--- src/main/java/com/ditto/service/MemberService.java
+++ src/main/java/com/ditto/service/MemberService.java
@@ -3,13 +3,17 @@ package com.ditto.service;
 import com.ditto.entity.Member;
 import com.ditto.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional //로직을 처리하다가 에러가발생하면 이전상태로 콜백시켜준다.
 @RequiredArgsConstructor //final 이나 @NonNull이 붙은 필드에 생성자를 생성해준다.
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -24,5 +28,20 @@ public class MemberService {
         if (findMember != null) {
             throw new IllegalStateException("이미 가입된 회원 입니다.");
         }
+    }
+
+    public UserDetails loadUserByUsername(String memberId) throws
+            UsernameNotFoundException { //로그인할 유저의 id를 파라미터로 전달받음
+        Member member = memberRepository.findByMemberId(memberId);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(memberId);
+        }
+
+        return User.builder() //User 객체 생성을 위해 회원의 아이디,비밀번호, role을 파라미터로 넘겨줌
+                .username(member.getMemberId())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
