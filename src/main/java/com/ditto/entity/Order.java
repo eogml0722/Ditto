4556e72a87e972ed9276f15ca27911dc25ba9811
이유주 <kikiblee@naver.com>
2023-12-10 21:15:24 +0900

    Revert "회원가입"
    This reverts commit 84f6efa7ce855f0c4aa034b748e028ebaa07a2ba.

--- src/main/java/com/ditto/entity/Order.java
+++ src/main/java/com/ditto/entity/Order.java
@@ -1,22 +1,22 @@
-package com.ditto.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Getter @Setter @ToString
-public class Order {
-    //주문
-    @Id
-    @Column(name = "order_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @OneToMany (mappedBy = "order")
-    private List<OrderItem> orderItemList = new ArrayList<>();
-}
+//package com.ditto.entity;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Getter @Setter @ToString
+//public class Order {
+//    //주문
+//    @Id
+//    @Column(name = "order_id")
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @OneToMany (mappedBy = "order")
+//    private List<OrderItem> orderItemList = new ArrayList<>();
+//}
