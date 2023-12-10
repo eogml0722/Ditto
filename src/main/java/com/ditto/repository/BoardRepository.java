93ed71deb37efd892b88addb2a4784c2c0b1b818
eogml0722 <eogml0722@naver.com>
2023-12-11 00:59:05 +0900

    게시글 수정을 위한 updateById 추가

--- src/main/java/com/ditto/repository/BoardRepository.java
+++ src/main/java/com/ditto/repository/BoardRepository.java
@@ -1,8 +1,10 @@
 package com.ditto.repository;
 
+import com.ditto.constant.BoardCategory;
 import com.ditto.entity.Board;
 import com.ditto.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
@@ -19,4 +21,19 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByMember(@Param("member") Member member,
                              @Param("title")String title,
                              @Param("content")String content);
+
+
+
+    //업데이트
+    @Query("UPDATE Board b " +
+            " SET b.title = :title," +
+            " b.content = :content," +
+            " b.boardCategory = :boardCategory" +
+            " WHERE b.id = :id")
+    @Modifying(clearAutomatically = true) //영속성 컨텍스트를 비워줌으로 DB에서 불러올때 일치시킴
+    void updateById(@Param("title")String title,
+                    @Param("content")String content,
+                    @Param("boardCategory")BoardCategory boardCategory,
+                    @Param("id")Long id);
+
 }
