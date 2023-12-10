fa9dc3f8776b465937faabff4bfc55c6fea6dbf8
eogml0722 <eogml0722@naver.com>
2023-12-10 22:52:41 +0900

    컨트롤러에 @RequestMapping(value = "/board") 추가

--- src/main/java/com/ditto/controller/BoardController.java
+++ src/main/java/com/ditto/controller/BoardController.java
@@ -6,20 +6,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.management.ValueExp;
 import javax.swing.*;
 
 @Controller
+@RequestMapping(value = "/board")
 @RequiredArgsConstructor
 public class BoardController {
 
-    @GetMapping(value = "/board")
+    @GetMapping(value = "/")
     public String goBoard(){
         return "board/menu";
     }
 
-    @GetMapping(value = "/board/create")
+    @GetMapping(value = "/create")
     public String goCreateBoard(Model model){
         model.addAttribute("boardDTO", new BoardDTO());
         return "board/createBoardForm";
@@ -27,7 +29,7 @@ public class BoardController {
 
 
 
-    @PostMapping(value = "/board/create")
+    @PostMapping(value = "/create")
     public String createBoard(Model model){
         return null;
     }
