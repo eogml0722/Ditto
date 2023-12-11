39ceef6a1422990391fd25b155914db73915d824
eogml0722 <eogml0722@naver.com>
2023-12-11 17:49:58 +0900

    소식 게시판 페이지 경로 설정

--- src/main/java/com/ditto/controller/BoardController.java
+++ src/main/java/com/ditto/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.ditto.controller;
 
 import com.ditto.dto.BoardDTO;
+import com.ditto.entity.Board;
+import com.ditto.repository.BoardRepository;
+import com.ditto.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,21 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.management.ValueExp;
 import javax.swing.*;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/board")
 @RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
+    private final BoardRepository boardRepository;
 
-    @GetMapping(value = "/")
-    public String goBoard(){
-        return "board/menu";
+    @GetMapping(value = "")
+    public String goBoard(Model model){
+        List<Board> boardList = boardRepository.findAll();
+        model.addAttribute("boardList", boardList);
+        return "/board/board";
     }
 
     @GetMapping(value = "/create")
     public String goCreateBoard(Model model){
         model.addAttribute("boardDTO", new BoardDTO());
-        return "board/createBoardForm";
+        return "createBoardForm";
     }
 
 
