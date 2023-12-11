ee7d1bac5c03b662660b905da9776848d7b2c281
eogml0722 <eogml0722@naver.com>
2023-12-11 14:41:49 +0900

    /board 경로 권한 추가

--- src/main/java/com/ditto/config/SecurityConfig.java
+++ src/main/java/com/ditto/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .mvcMatchers("/").permitAll()
-                .mvcMatchers("/css/**", "/js/**", "/members/**", "/img/**", "/extras/**").permitAll()
+                .mvcMatchers("/css/**", "/js/**", "/members/**", "/img/**", "/extras/**", "/board/**").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
