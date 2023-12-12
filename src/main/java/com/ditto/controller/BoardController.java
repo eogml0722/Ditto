02adc6b6965ed36db1061e4125ee60757f5dab74
eogml0722 <eogml0722@naver.com>
2023-12-12 17:47:17 +0900

    게시글 상세보기 페이지로 GetMapping 추가

--- src/main/java/com/ditto/controller/BoardController.java
+++ src/main/java/com/ditto/controller/BoardController.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.management.ValueExp;
 import javax.swing.*;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -28,6 +30,26 @@ public class BoardController {
         model.addAttribute("boardList", boardList);
         return "/board/board";
     }
+
+    @GetMapping(value = "/getBoard/{boardId}")
+    public String getBoard(@PathVariable("boardId") Long boardId , Model model) {
+        //경로로 board의 id를 받아왔다.
+        //받아온 id 와 일치하는 board.id 의 board를 dto로 보내준다.
+        Board board = boardService.getBoard(boardId);
+
+        BoardDTO boardDTO = BoardDTO.of(board);
+
+        //이전행
+        boardDTO.setBoardPrev(boardRepository.findLatestBoard(boardDTO.getId()));
+
+        //다음행
+//        boardDTO.setBoardNext(boardRepository.getNext(boardDTO.getId()));
+
+        model.addAttribute("boardDTO", boardDTO);
+
+        return "/board/getBoard";
+    }
+
 
     @GetMapping(value = "/create")
     public String goCreateBoard(Model model){
