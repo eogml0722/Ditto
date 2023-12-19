c0e8e9fc04c6fccd7000c3d87c09fb954c0e1a9b
eogml0722 <eogml0722@naver.com>
2023-12-19 17:45:38 +0900

    Board 뷰 DTO

--- src/main/java/com/ditto/dto/BoardFormDTO.java
+++ src/main/java/com/ditto/dto/BoardFormDTO.java
@@ -5,10 +5,12 @@ import com.ditto.entity.Board;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
-/*
+import java.util.Optional;
+
 @Getter @Setter @ToString
 public class BoardFormDTO {
 
@@ -19,5 +21,30 @@ public class BoardFormDTO {
     private String viewCount;
     private BoardCategory boardCategory;
 
+    //Auditing
+    private List<Board> boardList;
+    private LocalDateTime regTime;
+
+    //BoardFormDTO 로 분리
+
+    //검색필드
+    private String searchField;
+    private String searchOption;
+
+
+    //이전행
+    private Optional<Board> boardPrev;
+    //다음행
+    private Optional<Board> boardNext;
+
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public Board createBoard(){
+        return  modelMapper.map(this, Board.class );
+    }
+
+    public static BoardFormDTO of(Board board){
+        return modelMapper.map(board, BoardFormDTO.class);
+    }
 }
-*/
